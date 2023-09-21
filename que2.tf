488a74c9064252f43f96f65a8dbf32697dd4cf43
Iamneo <techadmin@iamneo.ai>
2023-09-21 05:26:12 +0000

    Backup Commit

--- que2.tf
+++ que2.tf
@@ -1,3 +1,15 @@
 terraform {
-    
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+        }
+    }
+}
+provider "aws" {
+    region = "ap-northeast-1"
+    access_key = "AKIAY6OXM4NMOHYBZ3HY"
+    secret_key = "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"
+}
+resource "aws_instance" "Ubuntu_server" {
+    ami = 
 }
