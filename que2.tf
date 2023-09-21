7d2da73aaa570b503d48fe9525c3e4f0c7a924c8
Iamneo <techadmin@iamneo.ai>
2023-09-21 05:31:37 +0000

    Backup Commit

--- que2.tf
+++ que2.tf
@@ -11,5 +11,9 @@ provider "aws" {
     secret_key = "UQOtj7MtYo7G6pudxiiqCkSIuehEhizm9ei9xcwk"
 }
 resource "aws_instance" "Ubuntu_server" {
-    ami = 
+    ami = "ami-0261755bbcb8c4a84"
+    instance_type = "t2.micro"
+}
+output "public_ip" {
+    value = 
 }
